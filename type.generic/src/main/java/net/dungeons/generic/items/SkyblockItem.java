09c1ed0edf3aaf93a187e73f50ae95d400d2e125
Kyrotechnics <141758940+Kyrotechnic@users.noreply.github.com>
2024-06-09 19:13:48 -0400

    Added custom minestom

--- type.generic/src/main/java/net/dungeons/generic/items/SkyblockItem.java
+++ type.generic/src/main/java/net/dungeons/generic/items/SkyblockItem.java
@@ -1,6 +1,73 @@
 package net.dungeons.generic.items;
 
+import net.kyori.adventure.nbt.CompoundBinaryTag;
+import net.minestom.server.component.DataComponent;
 import net.minestom.server.item.ItemStack;
+import net.minestom.server.item.Material;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class SkyblockItem extends ItemStack implements SItem {
+import java.util.function.Consumer;
+
+public abstract class SkyblockItem implements ItemStack, SItem {
+
+    @Override
+    public @NotNull Material material() {
+        return null;
+    }
+
+    @Override
+    public int amount() {
+        return 0;
+    }
+
+    @Override
+    public @NotNull ItemStack with(@NotNull Consumer<Builder> consumer) {
+        return null;
+    }
+
+    @Override
+    public @NotNull ItemStack withMaterial(@NotNull Material material) {
+        return null;
+    }
+
+    @Override
+    public @NotNull ItemStack withAmount(int i) {
+        return null;
+    }
+
+    @Override
+    public @NotNull <T> ItemStack with(@NotNull DataComponent<T> dataComponent, @NotNull T t) {
+        return null;
+    }
+
+    @Override
+    public @NotNull ItemStack without(@NotNull DataComponent<?> dataComponent) {
+        return null;
+    }
+
+    @Override
+    public @NotNull ItemStack consume(int i) {
+        return null;
+    }
+
+    @Override
+    public boolean isSimilar(@NotNull ItemStack itemStack) {
+        return false;
+    }
+
+    @Override
+    public @NotNull CompoundBinaryTag toItemNBT() {
+        return null;
+    }
+
+    @Override
+    public boolean has(@NotNull DataComponent<?> dataComponent) {
+        return false;
+    }
+
+    @Override
+    public <T> @Nullable T get(@NotNull DataComponent<T> dataComponent) {
+        return null;
+    }
 }
