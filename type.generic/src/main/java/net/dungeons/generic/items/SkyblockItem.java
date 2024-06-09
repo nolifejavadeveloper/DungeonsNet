8e8c82dd8ce09a032edb2b531034b6d3db439445
Kyrotechnics <141758940+Kyrotechnic@users.noreply.github.com>
2024-06-09 19:28:01 -0400

    sad

--- type.generic/src/main/java/net/dungeons/generic/items/SkyblockItem.java
+++ type.generic/src/main/java/net/dungeons/generic/items/SkyblockItem.java
@@ -9,65 +9,6 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Consumer;
 
-public class SkyblockItem implements ItemStack, SItem {
+public class SkyblockItem extends ItemStackImpl implements SItem {
 
-    @Override
-    public @NotNull Material material() {
-        return null;
-    }
-
-    @Override
-    public int amount() {
-        return 0;
-    }
-
-    @Override
-    public @NotNull ItemStack with(@NotNull Consumer<Builder> consumer) {
-        return null;
-    }
-
-    @Override
-    public @NotNull ItemStack withMaterial(@NotNull Material material) {
-        return null;
-    }
-
-    @Override
-    public @NotNull ItemStack withAmount(int i) {
-        return null;
-    }
-
-    @Override
-    public @NotNull <T> ItemStack with(@NotNull DataComponent<T> dataComponent, @NotNull T t) {
-        return null;
-    }
-
-    @Override
-    public @NotNull ItemStack without(@NotNull DataComponent<?> dataComponent) {
-        return null;
-    }
-
-    @Override
-    public @NotNull ItemStack consume(int i) {
-        return null;
-    }
-
-    @Override
-    public boolean isSimilar(@NotNull ItemStack itemStack) {
-        return false;
-    }
-
-    @Override
-    public @NotNull CompoundBinaryTag toItemNBT() {
-        return (CompoundBinaryTag) NBT_TYPE.write(this);
-    }
-
-    @Override
-    public boolean has(@NotNull DataComponent<?> dataComponent) {
-        return false;
-    }
-
-    @Override
-    public <T> @Nullable T get(@NotNull DataComponent<T> dataComponent) {
-
-    }
 }
